07a3427ffc7a289f59ecaf7d5837cc427ca232e3
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-04 17:30:04 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import styles from './style'
 import { Navbar, Hero, Stats,Business,Billing,CardDeal,Testimonials,Clients,CTA,Footer } from './components';
 
 const App = () => (
+  
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
